0648805ab9749427dcb4b93fc3fd598cb34d2ece
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-07-01 16:41:36 +0500

    Add Background and UI layers

---
+++ src/layers/BackgroundLayer.js
@@ -0,0 +1,23 @@
+/*
+Returns Background that is used
+to add background throughout the game
+*/
+const Background = (function() {
+
+  const Background = cc.Layer.extend({
+
+    ctor() {
+
+      this._super();
+      this.init()
+    },
+
+    init() {
+      const s = cc.winSize; //get size
+      const bg = cc.LayerColor.create(new cc.Color(255, 99, 71, 255), s.width, s.height);
+
+      this.addChild(bg, 0); //zIndex = 0
+    }
+  })
+  return Background;
+}());
---
+++ src/layers/UILayer.js
@@ -0,0 +1,45 @@
+/*
+Returns UI layer that is used to:
+  in IntroScene: starts new game
+  in GameScene: restarts current game with different tiles
+  in OutroScene: launches game again
+*/
+const UILayer = (function() {
+
+  const UILayer = cc.Layer.extend({
+
+    ctor() {
+
+      this._super();
+      this.createScreen();
+    },
+
+    // TODO: add best score/lastscore layouts
+
+    createScreen() {
+      const s = cc.winSize;
+
+      const startButton = new ccui.Button();
+      startButton.loadTextures(res.button, res.button);
+      startButton.setTitleText("Start new game");
+      startButton.setTitleFontName("Marvin");
+      startButton.setTitleFontSize(36);
+      startButton.setScale(0.4);
+      startButton.setPosition(s.width * 0.5, s.height * 0.15);
+      startButton.addTouchEventListener(this.buttonEvent, this);
+      this.addChild(startButton);
+    },
+
+    buttonEvent(sender, type) {
+      if(type == ccui.Widget.TOUCH_ENDED) {
+        const scale = new cc.ScaleBy(0.2, 0.8);
+        const easeScale = new cc.EaseBackOut(scale);
+        const reaction = new cc.CallFunc(cc.director.runScene(new cc.TransitionSlideInR(0.25, new GameScene())));
+        const chain = new cc.Sequence(easeScale, reaction);
+
+        this.runAction(chain);
+      }
+    }
+  })
+  return UILayer;
+}());
