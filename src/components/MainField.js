a3dc9bb0bd6668eb74be8b1db7c5f85f174e5f1d
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-07-04 20:39:47 +0500

    Split gamefield and Mainlayer into 2 different modules

---
+++ src/components/MainField.js
@@ -0,0 +1,179 @@
+const MainField = (function() {
+  const MainField = cc.Sprite.extend({
+
+    maxRows : 9, //Field size in number of rows
+    maxCols : 9, //and columns
+
+    tilesPos : null, // 2d array keeps tiles position
+    tilesSpr : null, // 2d array keeps tiles sprites
+
+    ctor() {
+      this._super(res.gamefield);
+      this.init();
+    },
+
+    init() {
+      const s = cc.winSize;
+
+      this.tilesPos = this.create2dArray(this.maxRows, this.maxCols, null);
+      this.tilesSpr = this.create2dArray(this.maxRows, this.maxCols, null);
+
+      //Initializing the game matrix and populating it with tiles
+      this.runAction(cc.CallFunc.create(this.initMatrix.bind(this)));
+    },
+
+    create2dArray(arow, acol, defValue) {
+      let arr = [];
+      for(let row = 0; row < arow; row++) {
+        arr[row] = [];
+        for(let col = 0; col < acol; col++) {
+          arr[row][col] = defValue;
+        }
+      }
+      return arr;
+    },
+
+    initMatrix() {
+
+      const half = 25;         //half size of a tile
+      const full = half * 2; //and full size of it
+
+      const baseX = 38;  //coords from which
+      const baseY = 38; //the matrix starts
+
+      //Assignment of position on matrix and sprite
+      for(let row = 0; row < this.maxRows; row++) {
+        for(let col = 0; col < this.maxRows; col++) {
+          this.tilesPos[row][col] = cc.p(baseX + col*full, baseY + row * full);
+          this.addOneTile(row, col);
+        }
+      }
+    },
+
+    addOneTile(row, col) {
+      const type = Math.floor(Math.random() * 5) + 1;
+
+      this.tilesSpr[row][col] = new cc.Sprite(`res/${type}.png`);
+      this.tilesSpr[row][col].setAnchorPoint(0.5, 0.5);
+      this.tilesSpr[row][col].extraAttr = type;
+      this.tilesSpr[row][col].rowIndex = row;
+      this.tilesSpr[row][col].colIndex = col;
+      this.tilesSpr[row][col].setPosition(this.tilesPos[row][col].x, this.tilesPos[row][col].y);
+      this.addChild(this.tilesSpr[row][col]);
+    },
+
+    tileExists(tile) {
+      return tile.rowIndex >= 0 && tile.rowIndex < 9 && tile.colIndex >= 0 && tile.colIndex < 9;
+    },
+
+    findTiles(tile) {
+      let neighbours = this.checkForColor(tile);
+
+      if(neighbours.length > 0) {
+        let returnArr = [tile];
+        while(neighbours.length > 0) {
+          for(let i = 0; i < neighbours.length; i++) {
+            let tilesToCheck = this.checkForColor(neighbours[i]);
+            let neighboursFiltered = tilesToCheck.filter(element => !neighbours.includes(element));
+            neighbours.push(...neighboursFiltered);
+            returnArr.push(...neighbours.splice(i, 1));
+          }
+        }
+        return returnArr;
+      }
+    },
+
+    checkForColor(tile) {
+      tile.isPicked = true;
+      const tilesToCheck = [];
+
+      const startRow = tile.rowIndex;
+      const startCol = tile.colIndex;
+
+      const upperTile = {rowIndex: startRow + 1, colIndex: startCol };
+      const lowerTile = {rowIndex: startRow - 1, colIndex: startCol };
+      const righterTile = {rowIndex: startRow, colIndex: startCol + 1};
+      const lefterTile = {rowIndex: startRow, colIndex: startCol - 1};
+
+      if(this.tileExists(upperTile)) {
+        tilesToCheck.push(upperTile);
+      }
+      if(this.tileExists(lowerTile)) {
+        tilesToCheck.push(lowerTile);
+      }
+      if(this.tileExists(righterTile)) {
+        tilesToCheck.push(righterTile);
+      }
+      if(this.tileExists(lefterTile)) {
+        tilesToCheck.push(lefterTile);
+      }
+
+      let neighbours = tilesToCheck
+          .map(element => this.tilesSpr[element.rowIndex][element.colIndex])
+          .filter(element =>element.extraAttr === tile.extraAttr)
+          .filter(tile => !tile.isPicked);
+
+      return neighbours;
+    },
+
+    destroyTiles(chunk) {
+      chunk.forEach(tile => {
+        let fly = new cc.MoveTo(0.4, 240, 700);
+        const deletion = new cc.CallFunc(tile => this.removeChild(tile), this);
+        const chain = new cc.Sequence(fly, deletion);
+        tile.runAction(chain);
+        this.tilesSpr[tile.rowIndex][tile.colIndex] = null;
+      });
+    },
+
+    whichTilesNeedMove() {
+      const tilesToMove = [];
+      for (let i = 1; i < this.maxRows; i++) {
+        for (let j = 0; j < this.maxCols; j++) {
+          if (this.tilesSpr[i][j] !== null) {
+            let emptySpace = 0;
+            for (let m = i - 1; m >= 0; m--) {
+              if (this.tilesSpr[m][j] === null) {
+                emptySpace++
+              }
+            }
+            if (emptySpace > 0) {
+              tilesToMove.push({ tile: this.tilesSpr[i][j], emptySpace });
+
+              this.tilesSpr[i - emptySpace][j] = this.tilesSpr[i][j];
+              this.tilesSpr[i - emptySpace][j].rowIndex = i - emptySpace;
+              this.tilesSpr[i][j] = null;
+            }
+          }
+        }
+      }
+      return tilesToMove;
+    },
+
+    tilesSlideDown() {
+      let tilesToMove = this.whichTilesNeedMove();
+
+      tilesToMove.forEach(movingTile => {
+        let tile = movingTile.tile;
+        let hole = movingTile.emptySpace;
+        let xCoord = tile.x;
+        let yCoord = tile.y - hole*50;
+        let slideDown = new cc.MoveTo(0.2, xCoord, yCoord);
+        tile.runAction(slideDown);
+      });
+    },
+
+    refillTiles() {
+      for(let i = 0; i < 9; i++) {
+        for(let j = 0; j < 9; j++) {
+          if(this.tilesSpr[i][j] == null) {
+            this.addOneTile(i, j)
+          }
+        }
+      }
+    },
+
+
+  })
+  return MainField;
+}());
--- src/layers/MainLayer.js
+++ src/layers/MainLayer.js
@@ -1,7 +1,6 @@
 const MainLayer = (function() {
   const MainLayer = cc.Layer.extend({
 
-
       maxRows : 9, //Field size in number of rows
       maxCols : 9, //and columns
 
@@ -16,138 +15,36 @@ const MainLayer = (function() {
     init() {
       const s = cc.winSize;
 
+      this.field = new MainField;
+      this.field.setPosition(s.width * 0.5, s.height * 0.5);
+      this.field.setAnchorPoint(0.5, 0.5);
+      this.addChild(this.field);
 
-
-      const gamefield = cc.Sprite.create(res.gamefield);
-      gamefield.setPosition(s.width * 0.5, s.height * 0.5);
-      this.addChild(gamefield);
-
-      const notification = NotificationCenter();
-      notification.addObserver(this, this.onClick, "click");
-
-
-
-      this.tilesPos = this.create2dArray(this.maxRows, this.maxCols, null);
-      this.tilesSpr = this.create2dArray(this.maxRows, this.maxCols, null);
-
-      //Initializing the game matrix and populating it with tiles
-      this.runAction(cc.CallFunc.create(this.initMatrix.bind(this)));
-
+      this.listener = cc.eventManager.addListener({
+        event: cc.EventListener.TOUCH_ONE_BY_ONE,
+        swallowTouches: true,
+        onTouchBegan: this.onClick}, this);
     },
 
-    create2dArray(arow, acol, defValue) {
-      let arr = [];
-      for(let row = 0; row < arow; row++) {
-        arr[row] = [];
-        for(let col = 0; col < acol; col++) {
-          arr[row][col] = defValue;
-        }
-      }
-      return arr;
+    onClick(touch, event) {
+      const location = touch.getLocation();
+      const target = event.getCurrentTarget();
+      const ly = Math.floor(location.y - 80);
+      const lx = Math.floor(location.x - 15);
+      const row =  Math.floor((ly - 80 + 25)/50);
+      const col =  Math.floor((lx - 15 + 25)/50);
+      const tile = target.field.tilesSpr[row][col];
+
+      const arrOfTiles = target.field.findTiles(tile);
+      target.makeTurn(arrOfTiles);
     },
 
-    initMatrix() {
+    makeTurn(arr) {
+      this.field.destroyTiles(arr);
+      this.field.tilesSlideDown();
+      this.field.refillTiles();
+    }
 
-      const half = 25;         //half size of a tile
-      const full = half * 2; //and full size of it
-
-      const baseX = 40;  //coords from which
-      const baseY = 160; //the matrix starts
-
-      //Assignment of position on matrix and sprite
-      for(let row = 0; row < this.maxRows; row++) {
-        for(let col = 0; col < this.maxRows; col++) {
-          this.tilesPos[row][col] = cc.p(baseX + col*full, baseY + row * full);
-          this.addOneTile(row, col);
-        }
-      }
-    },
-
-    addOneTile(row, col) {
-      const type = Math.floor(Math.random() * 5) + 1;
-
-      this.tilesSpr[row][col] = new TileSprite(type);
-      this.tilesSpr[row][col].setAnchorPoint(0.5, 0.5);
-      this.tilesSpr[row][col].extraAttr = type;
-      this.tilesSpr[row][col].rowIndex = row;
-      this.tilesSpr[row][col].colIndex = col;
-      this.tilesSpr[row][col].setPosition(this.tilesPos[row][col].x, this.tilesPos[row][col].y);
-
-      this.addChild(this.tilesSpr[row][col]);
-    },
-
-    onClick(target){
-      let arrOfTiles = this.findTiles(target);
-      this.destroyTiles(arrOfTiles);
-
-    },
-
-    tileExists(tile) {
-      return tile.rowIndex >= 0 && tile.rowIndex < 9 && tile.colIndex >= 0 && tile.colIndex < 9;
-    },
-
-    findTiles(tile) {
-      let neighbours = this.checkForColor(tile);
-
-      if(neighbours.length > 0) {
-        let returnArr = [tile];
-        while(neighbours.length > 0) {
-          for(let i = 0; i < neighbours.length; i++) {
-            let tilesToCheck = this.checkForColor(neighbours[i]);
-            let neighboursFiltered = tilesToCheck.filter(element => !neighbours.includes(element));
-            neighbours.push(...neighboursFiltered);
-            returnArr.push(...neighbours.splice(i, 1));
-          }
-        }
-        return returnArr;
-      }
-    },
-
-    checkForColor(tile) {
-      tile.isPicked = true;
-      const tilesToCheck = [];
-
-      const startRow = tile.rowIndex;
-      const startCol = tile.colIndex;
-
-      const upperTile = {rowIndex: startRow + 1, colIndex: startCol };
-      const lowerTile = {rowIndex: startRow - 1, colIndex: startCol };
-      const righterTile = {rowIndex: startRow, colIndex: startCol + 1};
-      const lefterTile = {rowIndex: startRow, colIndex: startCol - 1};
-
-      if(this.tileExists(upperTile)) {
-        tilesToCheck.push(upperTile);
-      }
-      if(this.tileExists(lowerTile)) {
-        tilesToCheck.push(lowerTile);
-      }
-      if(this.tileExists(righterTile)) {
-        tilesToCheck.push(righterTile);
-      }
-      if(this.tileExists(lefterTile)) {
-        tilesToCheck.push(lefterTile);
-      }
-
-      let neighbours = tilesToCheck
-          .map(element => this.tilesSpr[element.rowIndex][element.colIndex])
-          .filter(element =>element.extraAttr === tile.extraAttr)
-          .filter(tile => !tile.isPicked);
-
-      return neighbours;
-    },
-
-    destroyTiles(chunk) {
-      console.log(chunk);
-      chunk.forEach(tile => {
-        let fly = new cc.MoveTo(0.3, 240, 720);
-        const deletion = new cc.CallFunc(tile => this.removeChild(tile), this);
-        const chain = new cc.Sequence(fly, deletion);
-        tile.runAction(chain);
-        this.tilesSpr[tile.rowIndex][tile.colIndex] = null;
-      });
-    },
-
-
-  })
+  });
   return MainLayer;
 }());
