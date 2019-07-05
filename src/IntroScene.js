fdf1062f07bfe74deda91458cefd54bbdd43c368
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-07-05 20:33:06 +0500

    Intro scene is now is now Outro scene as well

--- src/IntroScene.js
+++ src/IntroScene.js
@@ -6,19 +6,30 @@ will show:
 3. Button to start the game
 */
 const IntroScene = (function() {
-  //TODO: get scores
 
   const IntroScene = cc.Scene.extend({
-    ctor() {
+    ctor(result = "Orange BLAST!!!") {
       this._super();
       this.addChild(new Background(), 0); //zIndex = 0
       this.addChild(new UILayer(), 1); //zIndex = 1
+      this.storage = new Storage;
+      this.init(result);
+    },
 
-      const title = new cc.LabelTTF("Orange BLAST!!!", "Marvin", 32);
+    init(result) {
       const s = cc.winSize;
+
+      const title = new cc.LabelTTF(result, "Marvin", 32);
+      const best = new cc.LabelTTF(`Best score is: ${this.storage.bestScore}`, "Marvin", 26);
+      const last = new cc.LabelTTF(`Last score is: ${this.storage.lastScore}`, "Marvin", 26);
+
       title.setPosition(s.width * 0.5, s.height * 0.8);
+      best.setPosition(s.width * 0.5, s.height * 0.6);
+      last.setPosition(s.width * 0.5, s.height * 0.5);
 
       this.addChild(title);
+      this.addChild(last);
+      this.addChild(best);
     }
   });
   return IntroScene;
