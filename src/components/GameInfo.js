01b24090f77d2c4f31a254a9058a0d10ec570e00
Danil 'Addeu' Deulin <addeulin@gmail.com>
2019-07-06 11:45:05 +0500

    Minor changes + comments 1. Point awarding scheme changed 2. Pharse on lost changed

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -17,6 +17,11 @@ const GameInfo = (function() {
       this.turnsLabel = new cc.LabelTTF(`Turns left: ${this.turns}`, "Marvin", 20);
     }
 
+    /**
+    Updates game score according to the size of deleted tiles chunk
+    the bigger the chunk the more points the player recieves
+    @param {number} Array.length of tiles to delete from MainLayer
+    */
     updateScore(number) {
       if(number <= 3) {
         this.score += number * 20;
@@ -27,7 +32,7 @@ const GameInfo = (function() {
         this.scoreLabel.setString(`${this.score} out of ${this.goal}`);
       }
       if (5 < number) {
-        this.score += number * 50;
+        this.score += number * 40;
         this.scoreLabel.setString(`${this.score} out of ${this.goal}`);
       }
     }
@@ -37,10 +42,16 @@ const GameInfo = (function() {
       this.turnsLabel.setString(`Turns left: ${this.turns}`);
     }
 
+    /*
+    Conditions to finish the game.
+    If the player gets more than 10000 points before
+    runs out of turns, it will be rewarded with bonus points
+    that equal number of turns left multiplie by 500
+    */
     isOver() {
       if(this.turns <= 0) {
         this.storage.setLastScore(this.score);
-        cc.director.runScene(new cc.TransitionSlideInR(0.4, new IntroScene("You lost!")));
+        cc.director.runScene(new cc.TransitionSlideInR(0.4, new IntroScene("Ooops! Try again!")));
       }
       else if (this.score >= 10000) {
         this.score += this.turns * 500;
